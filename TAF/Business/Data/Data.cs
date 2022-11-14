29bfa80857285cc96af65a16d9504a7964c31a23
Srul1k <72085557+Srul1k@users.noreply.github.com>
2022-11-14 18:16:24 +0300

    Add API Tests

--- TAF/Business/Data/Data.cs
+++ TAF/Business/Data/Data.cs
@@ -2,6 +2,7 @@
 {
     public static class Data
     {
+        public const string APIUrl = "https://jsonplaceholder.typicode.com";
         public const string ApplicationUrl = "https://www.epam.com";
         public const string NameOfFileToDownload = "EPAM_Systems_Company_Overview.pdf";
         public static string DownloadFolder = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads";
